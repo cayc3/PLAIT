949ae3cc36514cf4d3e24298e7d96e8b58d012a8
cayc3 <cayce@tutanota.com>
2017-06-05 18:34:11 +0700

    Update detail.py

--- src/UILib/detail.py
+++ src/UILib/detail.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -15,64 +15,64 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(495, 500)
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
         self.verticalLayout_2.setSpacing(3)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        self.groupBox = QtGui.QGroupBox(Dialog)
+        self.groupBox = QtWidgets.QGroupBox(Dialog)
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.groupBox)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.groupBox)
         self.horizontalLayout.setContentsMargins(3, 6, 3, 3)
         self.horizontalLayout.setSpacing(3)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.tabWidget = QtGui.QTabWidget(self.groupBox)
+        self.tabWidget = QtWidgets.QTabWidget(self.groupBox)
         self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
-        self.tab = QtGui.QWidget()
+        self.tab = QtWidgets.QWidget()
         self.tab.setObjectName(_fromUtf8("tab"))
-        self.horizontalLayout_3 = QtGui.QHBoxLayout(self.tab)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.tab)
         self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
-        self.listWidget = QtGui.QListWidget(self.tab)
+        self.listWidget = QtWidgets.QListWidget(self.tab)
         self.listWidget.setObjectName(_fromUtf8("listWidget"))
         self.horizontalLayout_3.addWidget(self.listWidget)
         self.tabWidget.addTab(self.tab, _fromUtf8(""))
-        self.tab_4 = QtGui.QWidget()
+        self.tab_4 = QtWidgets.QWidget()
         self.tab_4.setObjectName(_fromUtf8("tab_4"))
-        self.horizontalLayout_6 = QtGui.QHBoxLayout(self.tab_4)
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout(self.tab_4)
         self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
-        self.treeWidget = QtGui.QTreeWidget(self.tab_4)
+        self.treeWidget = QtWidgets.QTreeWidget(self.tab_4)
         self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
         self.treeWidget.headerItem().setText(0, _fromUtf8("1"))
         self.horizontalLayout_6.addWidget(self.treeWidget)
         self.tabWidget.addTab(self.tab_4, _fromUtf8(""))
-        self.tab_3 = QtGui.QWidget()
+        self.tab_3 = QtWidgets.QWidget()
         self.tab_3.setObjectName(_fromUtf8("tab_3"))
-        self.horizontalLayout_5 = QtGui.QHBoxLayout(self.tab_3)
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.tab_3)
         self.horizontalLayout_5.setObjectName(_fromUtf8("horizontalLayout_5"))
-        self.tableWidget = QtGui.QTableWidget(self.tab_3)
+        self.tableWidget = QtWidgets.QTableWidget(self.tab_3)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
         self.tableWidget.setColumnCount(5)
         self.tableWidget.setRowCount(0)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, item)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, item)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(3, item)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(4, item)
         self.tableWidget.verticalHeader().setVisible(False)
         self.horizontalLayout_5.addWidget(self.tableWidget)
@@ -87,10 +87,14 @@ class Ui_Dialog(object):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "详细信息", None))
-        self.groupBox.setTitle(_translate("Dialog", "文件信息", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Dialog", "基本信息", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("Dialog", "导入表信息", None))
+        #Dialog.setWindowTitle(_translate("Dialog", "详细信息", None))
+        Dialog.setWindowTitle(_translate("Dialog", "More", None))
+        #self.groupBox.setTitle(_translate("Dialog", "文件信息", None))
+        self.groupBox.setTitle(_translate("Dialog", "File", None))
+        #self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Dialog", "基本信息", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Dialog", "Basic", None))
+        #self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("Dialog", "导入表信息", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("Dialog", "Import Table", None))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("Dialog", "Name", None))
         item = self.tableWidget.horizontalHeaderItem(1)
@@ -101,15 +105,15 @@ class Ui_Dialog(object):
         item.setText(_translate("Dialog", "RawSize", None))
         item = self.tableWidget.horizontalHeaderItem(4)
         item.setText(_translate("Dialog", "Entropy", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Dialog", "PE节信息", None))
+        #self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Dialog", "PE节信息", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Dialog", "PE Section", None))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    Dialog = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-
