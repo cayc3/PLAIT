f30fd1cff93901d260cfc1ce6786e872a6251ea8
cayc3 <cayce@tutanota.com>
2017-06-05 18:56:36 +0700

    Update whitelist.py

--- src/UILib/whitelist.py
+++ src/UILib/whitelist.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -15,31 +15,31 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(363, 458)
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.textBrowser = QtGui.QTextBrowser(Dialog)
+        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
         self.textBrowser.setReadOnly(False)
         self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
         self.verticalLayout.addWidget(self.textBrowser)
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.PB_Apply = QtGui.QPushButton(Dialog)
+        self.PB_Apply = QtWidgets.QPushButton(Dialog)
         self.PB_Apply.setObjectName(_fromUtf8("PB_Apply"))
         self.horizontalLayout.addWidget(self.PB_Apply)
-        self.PB_OK = QtGui.QPushButton(Dialog)
+        self.PB_OK = QtWidgets.QPushButton(Dialog)
         self.PB_OK.setObjectName(_fromUtf8("PB_OK"))
         self.horizontalLayout.addWidget(self.PB_OK)
         self.verticalLayout.addLayout(self.horizontalLayout)
@@ -49,16 +49,18 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "白名单设置", None))
+        Dialog.setWindowTitle(_translate("Dialog", "Whitelist Settings", None))
         self.PB_Apply.setText(_translate("Dialog", "应  用", None))
+        self.PB_Apply.setText(_translate("Dialog", "Enable", None))
         self.PB_OK.setText(_translate("Dialog", "确  定", None))
+        self.PB_OK.setText(_translate("Dialog", "OK", None))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    Dialog = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-
