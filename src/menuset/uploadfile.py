25a210dfb458fa9edb05728eb91515722b0f119f
cayc3 <cayce@tutanota.com>
2017-06-05 19:27:01 +0700

    Update uploadfile.py

--- src/menuset/uploadfile.py
+++ src/menuset/uploadfile.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 
-from PyQt4 import QtGui, QtCore, Qt
+from PyQt5 import QtGui, QtCore, QtWidgets
 import sys, os, time
 sys.path.append("..")
 from UILib.upload_file import Ui_Dialog
@@ -9,31 +9,34 @@ from advanceoperate.uploadthread import UploadFile, AddFileToQqueu
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-class Dialog(QtGui.QDialog):
+class Dialog(QtWidgets.QDialog):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.filename = ""
-        self.apikey = "632d8d808a60dba0ac32496f6c41073fff34e7ec6b33723136c7ca1861cd5ae6"
+        self.apikey = ""
 
         self.table = self.ui.tableWidget
-        
-        # 按键信号槽
+
+        # 按键信号槽/Key Signal Slot
         self.ui.PB_Upload.clicked.connect(self.uploadFile)
         self.ui.PB_2Queue.clicked.connect(self.addFile2Queue)
 
     '''
-    接收ui窗口传值
+    #接收ui窗口传值
+    Receive UI Window Values
     '''
     def getFilename(self, filename):
         print "get filename from main ui"
         self.filename = filename
 
     '''
-    点击上传文件按钮事件
-    连接uploadfile中的线程函数
+    #点击上传文件按钮事件
+    Click Upload File Button Event
+    #连接uploadfile中的线程函数
+    Threading Function in Connection UploadFile
     '''
     def uploadFile(self, filename):
         self.clearFileData()
@@ -41,15 +44,19 @@ class Dialog(QtGui.QDialog):
         self.uploadthread = UploadFile(self.filename, self.apikey)
         self.uploadthread.finishSignal.connect(self.recvAnalyzeResult)
         self.uploadthread.start()
-    
+
     '''
-    接收分析结果函数
-    接收一个元组内容
+    #接收分析结果函数
+    Receive Analysis Result Functions
+    #接收一个元组内容
+    Receive a Meta-Group Content
     '''
     def recvAnalyzeResult(self, index, msg):
         if 1 == index:
-            self.ui.label_4.setText(u"网络响应: " + str(msg[2]))
-            self.ui.label_5.setText(u"返回代码: " + str(msg[3]))
+            #self.ui.label_4.setText(u"网络响应: " + str(msg[2]))
+            self.ui.label_4.setText(u"Network Response: " + str(msg[2]))
+            #self.ui.label_5.setText(u"返回代码: " + str(msg[3]))
+            self.ui.label_5.setText(u"Return Code: " + str(msg[3]))
             self.ui.PB_Upload.setEnabled(True)
             # self.ui.LE_HttpCode.setText(str(msg[2]))
             # self.ui.LE_SerCode.setText(str(msg[3]))
@@ -64,25 +71,27 @@ class Dialog(QtGui.QDialog):
                 i = 0
                 for n in msg[1]:
                     n = n.split("^")
-                    engine = QtGui.QTableWidgetItem(n[0]) # 引擎
-                    result = QtGui.QTableWidgetItem(n[1]) # 结果
+                    engine = QtWidgets.QTableWidgetItem(n[0]) # 引擎/Engine
+                    result = QtWidgets.QTableWidgetItem(n[1]) # 结果/Results
                     self.table.setItem(i, 0, engine)
                     self.table.setItem(i, 1, result)
                     if str(n[1]) != " None":
                         detecten = detecten + 1
-                        self.table.item(i, 1).setForeground(Qt.Qt.red)
+                        self.table.item(i, 1).setForeground(QtCore.Qt.red)
                     else:
-                        self.table.item(i, 1).setForeground(Qt.Qt.green)
+                        self.table.item(i, 1).setForeground(QtCore.Qt.green)
                     i = i + 1
                 linetext = str(detecten) + "/" + str(rowcount)
                 # self.ui.LE_DeteRate.setText(linetext)
-                self.ui.label.setText(u"检测率: " + linetext)
+                #self.ui.label.setText(u"检测率: " + linetext)
+                self.ui.label.setText(u"Detection Rate: " + linetext)
             elif msg[0] == "permalink":
                 if msg[1]:
                     print "your file has been analysising"
                     self.ui.LE_URL.setText(str(msg[1][0]))
             elif msg[0] == "http_code":
                 self.ui.LE_URL.setText(u"网络响应问题，请参考官方文档")
+                self.ui.LE_URL.setText(u"Network Response Problems，Please Refer to the Official Document")
         if 2 == index:
             if msg[0] == "baseinfo":
                 for n in msg[1]:
@@ -90,25 +99,29 @@ class Dialog(QtGui.QDialog):
                     self.ui.listWidget.addItem(n)
         if 3 == index:
             self.ui.PB_Upload.setEnabled(True)
-            self.ui.LE_URL.setText(u"网络连接失败...")
+            #self.ui.LE_URL.setText(u"网络连接失败...")
+            self.ui.LE_URL.setText(u"Network Connection Failed ...")
 
     def addFile2Queue(self, filename):
         pass
-    
+
     def clearFileData(self):
         print "clear table data"
         self.table.setRowCount(0)
         self.table.clearContents()
         self.ui.listWidget.clear()
         self.ui.LE_URL.clear()
-        self.ui.label.setText(u"检测率: ")
-        self.ui.label_4.setText(u"网络响应: ")
-        self.ui.label_5.setText(u"返回代码: ")
+        #self.ui.label.setText(u"检测率: ")
+        self.ui.label.setText(u"Detection Rate: ")
+        #self.ui.label_4.setText(u"网络响应: ")
+        self.ui.label_4.setText(u"Network Response: ")
+        #self.ui.label_5.setText(u"返回代码: ")
+        self.ui.label_5.setText(u"Return Code: ")
 
 
 if __name__ == "__main__":
 
-    window = QtGui.QApplication(sys.argv)
+    window = QtWidgets.QApplication(sys.argv)
     thiswindow = Dialog()
     thiswindow.show()
 
