a1898ec8a3d7c1816ef7739f719c3f8413189655
cayc3 <cayce@tutanota.com>
2017-06-05 19:23:24 +0700

    Update setting.py

--- src/menuset/setting.py
+++ src/menuset/setting.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 
-from PyQt4 import QtGui, QtCore, Qt
+from PyQt5 import QtGui, QtCore, QtWidgets
 import sys, os, time
 sys.path.append("..")
 from UILib.setting import Ui_Dialog
@@ -10,26 +10,50 @@ from globalset import FlagSet
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-class Dialog(QtGui.QDialog):
+class Dialog(QtWidgets.QDialog):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.ui.pushButton.clicked.connect(self.updatesystem)
         self.ui.pushButton_2.clicked.connect(self.updateyara)
+        self.ui.pushButton_3.clicked.connect(self.updateclam)
 
-    def updateyara(self):
-        update = QtGui.QMessageBox()
-        recv = update.question(self, u"更新", u"更新规则库时会停止扫描，是否更新", update.Yes, update.No)
+    def updatesystem(self):
+        update = QtWidgets.QMessageBox()
+        #recv = update.question(self, u"更新", u"更新规则库时会停止扫描，是否更新", update.Yes, update.No)
+        recv = update.question(self, u"Update", u"Scanning is Stopped while Updating. \n \n Update Now?", update.Yes, update.No)
         if recv == update.Yes:
             FlagSet.scanstopflag = 0
             U = UpdateData()
-            U.updateYaraData()
+            U.pullApp()
+
+    def updateyara(self):
+        update = QtWidgets.QMessageBox()
+        #recv = update.question(self, u"更新", u"更新规则库时会停止扫描，是否更新", update.Yes, update.No)
+        recv = update.question(self, u"Update", u"Scanning is Stopped while Updating. \n \n Update Now?", update.Yes, update.No)
+        if recv == update.Yes:
+            FlagSet.scanstopflag = 0
+            U = UpdateData()
+            U.cloneYaraData()
+            #U.updateYaraData()
+
+    def updateclam(self):
+        update = QtWidgets.QMessageBox()
+        #recv = update.question(self, u"更新", u"更新规则库时会停止扫描，是否更新", update.Yes, update.No)
+        recv = update.question(self, u"Update", u"Scanning is Stopped while Updating.\n \n Update Now?", update.Yes, update.No)
+        if recv == update.Yes:
+            FlagSet.scanstopflag = 0
+            U = UpdateData()
+            U.cloneClamData()
+            #U.updateClamData()
+
 
 if __name__ == "__main__":
 
-    window = QtGui.QApplication(sys.argv)
+    window = QtWidgets.QApplication(sys.argv)
     thiswindow = Dialog()
     thiswindow.show()
 
