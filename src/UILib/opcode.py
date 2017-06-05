1f83b12cf4ce48150a94a5a7025ce3987e04587f
cayc3 <cayce@tutanota.com>
2017-06-05 18:46:53 +0700

    Update opcode.py

--- src/UILib/opcode.py
+++ src/UILib/opcode.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -15,54 +15,54 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(348, 488)
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.splitter = QtGui.QSplitter(Dialog)
+        self.splitter = QtWidgets.QSplitter(Dialog)
         self.splitter.setOrientation(QtCore.Qt.Vertical)
         self.splitter.setObjectName(_fromUtf8("splitter"))
-        self.groupBox_2 = QtGui.QGroupBox(self.splitter)
+        self.groupBox_2 = QtWidgets.QGroupBox(self.splitter)
         self.groupBox_2.setMinimumSize(QtCore.QSize(0, 0))
         self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.groupBox_2)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        self.tableWidget = QtGui.QTableWidget(self.groupBox_2)
+        self.tableWidget = QtWidgets.QTableWidget(self.groupBox_2)
         self.tableWidget.setRowCount(0)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
         self.tableWidget.setColumnCount(2)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtGui.QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, item)
         self.tableWidget.horizontalHeader().setDefaultSectionSize(150)
         self.tableWidget.verticalHeader().setVisible(False)
         self.verticalLayout_2.addWidget(self.tableWidget)
-        self.groupBox = QtGui.QGroupBox(self.splitter)
+        self.groupBox = QtWidgets.QGroupBox(self.splitter)
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.verticalLayout_3 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.groupBox)
         self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
-        self.label = QtGui.QLabel(self.groupBox)
+        self.label = QtWidgets.QLabel(self.groupBox)
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout_3.addWidget(self.label)
-        self.label_2 = QtGui.QLabel(self.groupBox)
+        self.label_2 = QtWidgets.QLabel(self.groupBox)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.verticalLayout_3.addWidget(self.label_2)
-        self.label_3 = QtGui.QLabel(self.groupBox)
+        self.label_3 = QtWidgets.QLabel(self.groupBox)
         self.label_3.setText(_fromUtf8(""))
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.verticalLayout_3.addWidget(self.label_3)
         self.verticalLayout.addWidget(self.splitter)
-        self.progressBar = QtGui.QProgressBar(Dialog)
+        self.progressBar = QtWidgets.QProgressBar(Dialog)
         self.progressBar.setProperty("value", 0)
         self.progressBar.setTextVisible(False)
         self.progressBar.setObjectName(_fromUtf8("progressBar"))
@@ -72,23 +72,27 @@ class Ui_Dialog(object):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "操作码分类", None))
-        self.groupBox_2.setTitle(_translate("Dialog", "操作码序列", None))
+        #Dialog.setWindowTitle(_translate("Dialog", "操作码分类", None))
+        Dialog.setWindowTitle(_translate("Dialog", "OpCode Classification", None))
+        #self.groupBox_2.setTitle(_translate("Dialog", "操作码序列", None))
+        self.groupBox_2.setTitle(_translate("Dialog", "OpCode Sequences", None))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("Dialog", "n-gram opcode", None))
         item = self.tableWidget.horizontalHeaderItem(1)
         item.setText(_translate("Dialog", "frequency", None))
-        self.groupBox.setTitle(_translate("Dialog", "分类结果", None))
-        self.label.setText(_translate("Dialog", "随机森林训练结论： ", None))
-        self.label_2.setText(_translate("Dialog", "弱分类器概率分布： ", None))
+        #self.groupBox.setTitle(_translate("Dialog", "分类结果", None))
+        self.groupBox.setTitle(_translate("Dialog", "Classification Results", None))
+        #self.label.setText(_translate("Dialog", "随机森林训练结论： ", None))
+        self.label.setText(_translate("Dialog", "Random Forest Training Results： ", None))
+        #self.label_2.setText(_translate("Dialog", "弱分类器概率分布： ", None))
+        self.label_2.setText(_translate("Dialog", "Probability Distribution of Weak Classifiers： ", None))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    Dialog = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-
