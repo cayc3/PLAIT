2c0e7ba86f357aacf4249e7aed802343180da9c9
cayc3 <cayce@tutanota.com>
2017-06-05 19:17:10 +0700

    Update authorinfo.py

--- src/menuset/authorinfo.py
+++ src/menuset/authorinfo.py
@@ -1,15 +1,15 @@
 #coding=utf-8
 
-from PyQt4 import QtGui, QtCore, Qt
+from PyQt5 import QtGui, QtCore, QtWidgets
 import sys, os
 from PIL import Image
 sys.path.append("..")
 from UILib.author import Ui_Dialog
 
-class Dialog(QtGui.QDialog):
+class Dialog(QtWidgets.QDialog):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
@@ -24,11 +24,11 @@ class Dialog(QtGui.QDialog):
             self.ui.label.setPixmap(QtGui.QPixmap.fromImage(image))
             self.ui.label_2.setText(u"  bugreport@ ")
             self.ui.label_3.setText(u"  zengrx1995@gmail.com; zengrx1995@163.com; zengrx1995@outlook.com")
-        
+
 
 if __name__ == "__main__":
 
-    window = QtGui.QApplication(sys.argv)
+    window = QtWidgets.QApplication(sys.argv)
     thiswindow = Dialog()
     thiswindow.show()
 
